091b9327cba2ab77d95c194929403f9573f278c7
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-09 22:06:22 +0700

    Membuat app router untuk setiap path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},  // proteksi route: hanya bisa diakses jika sudah login
+];
