461bdfc3b1d32a4af6de4e0be52c06abb826102a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-09 22:03:09 +0700

    Menyediakan routing berdasarkan route dan menyediakan http client

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; // komponen utama aplikasi
+import { provideRouter } from '@angular/router'; // untuk menyediakan routing ke aplikasi
+import { routes } from './app/app.routes';  // impor definisi route
+import { provideHttpClient } from '@angular/common/http'; // mengaktifkan fitur HttpClient (untuk request API)
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), // menyediakan routing berdasarkan route yang sudah ditentukan
+    provideHttpClient()    // menyediakan HttpClient agar bisa dipakai di seluruh aplikasi
+  ]
+});
