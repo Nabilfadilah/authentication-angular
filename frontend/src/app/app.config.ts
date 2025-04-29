1174d54d3efec6769f3deb7b4fefecc5a5b12cde
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-29 10:28:46 +0700

    Memberikan konfigurasi

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    // optimasi perubahan zona: menggabungkan beberapa event DOM menjadi satu tick Angular, untuk meningkatkan performa.
+
+    provideRouter(routes), 
+    // mendaftarkan konfigurasi routing aplikasi dengan menggunakan 'routes' yang sudah didefinisikan.
+
+    provideClientHydration(withEventReplay()),
+    // membantu mendukung SSR (Server Side Rendering) dengan fitur 'event replay' untuk menjaga interaktivitas aplikasi.
+
+    provideHttpClient(withInterceptorsFromDi())
+    // mengaktifkan HTTP Client bawaan Angular dan mengizinkan penggunaan interceptor yang diambil dari Dependency Injection (DI).
+  ]
 };
