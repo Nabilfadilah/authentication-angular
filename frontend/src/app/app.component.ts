4a2704e2e228f7ff5c6639615f501f2d389c0252
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-29 10:23:31 +0700

    Menambah state dan toggle untuk buka/tutup sidebar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; // untuk router-outlet HTML
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,                 // nama selector yang digunakan
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent], // impor RouterOutlet untuk digunakan di template
+  templateUrl: './app.component.html',  // template HTML dari komponen ini
+  styleUrls: ['./app.component.css']    // file CSS untuk styling
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend'; // properti title (opsional)
+
+  isSidebarOpen = true; // tambah state buat sidebar
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen; // toggle buka/tutup
+  }
 }
